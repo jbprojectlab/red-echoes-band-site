628798eb009dfbf4ac93eb8b3a0cbd0098c2fe07
jbprojectlab <james.byrd1265@gmail.com>
2019-09-30 19:54:59 -0400

    feat: play/pause button toggle

--- src/components/song.jsx
+++ src/components/song.jsx
@@ -1,17 +1,22 @@
-import React, {useState} from 'react';
+import React, {useRef} from 'react';
 import {Howl, Howler} from 'howler';
 import '../css/song.css';
 Howler.autoUnlock = false;
 
 const Song = ({title, src, length}) => {
+  const imgRef = useRef('pause')
   let sound = new Howl({src, html5: true, preload: true, paused: true});
-  const playSong = () => {
+  const toggleSongState = () => {
     sound = sound || new Howl({src, html5: true, preload: true, paused: true});
-    if(sound.playing()) {
-      sound.pause()
+    if (sound.playing()) {
+      sound.pause();
     } else {
       sound.play();
     }
+  };
+  const playSong = () => {
+    toggleSongState()
+    imgRef.current.src = imgRef.current.src.includes(`/assets/img/play-circle-fill.png`) ? `/assets/img/pause-circle-fill.png` : `/assets/img/play-circle-fill.png`
   }
   return (
     <div className="song bg-blk white brdr-white flex align-ctr space-bw pdg-40">
@@ -19,8 +24,11 @@ const Song = ({title, src, length}) => {
         <h4>{title}</h4>
         <p className="lighter">{length}</p>
       </span>
-      <button className={`play-btn bg-gray white brdr-white`} onClick={playSong}>
-        <img src={`/assets/img/play-circle-fill.png`} />
+      <button
+        className={`play-btn bg-gray white brdr-white`}
+        onClick={playSong}
+      >
+        <img ref={imgRef} src={`/assets/img/play-circle-fill.png`} />
       </button>
     </div>
   );
