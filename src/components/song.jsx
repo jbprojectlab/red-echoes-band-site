a4122f1ad28bda8967f6907ada4e109f90822822
jbprojectlab <james.byrd1265@gmail.com>
2019-09-30 21:38:46 -0400

    feat: reset play button onstop

--- src/components/song.jsx
+++ src/components/song.jsx
@@ -1,20 +1,20 @@
 import React, {useRef} from 'react';
 import {Howl, Howler} from 'howler';
 import '../css/song.css';
-const stopCurrentlyPlayingSongs = (song) => {
+const stopCurrentlyPlayingSongs = song => {
   Howler._howls.forEach(howl => {
     if (howl.playing() && howl !== song) {
       howl.stop();
-      // howl.unload()
     }
   });
 };
 
 const Song = ({title, src, length}) => {
   Howler.autoUnlock = false;
-  const options = {src, html5: true, preload: true, paused: true};
+  const options = {src, html5: true, preload: true, paused: true, onstop: () => togglePlayButton()};
   const imgRef = useRef('pause');
   let sound = new Howl(options);
+
   const toggleSongState = () => {
     sound = sound || new Howl(options);
     stopCurrentlyPlayingSongs(sound);
@@ -24,12 +24,18 @@ const Song = ({title, src, length}) => {
       sound.play();
     }
   };
-  const playSong = () => {
-    toggleSongState();
+
+  const togglePlayButton = () => {
     imgRef.current.src = sound.playing()
       ? `/assets/img/pause-circle-fill.png`
-      : `/assets/img/play-circle-fill.png`;
+      : `/assets/img/play-circle-fill.png`;    
+  }
+
+  const playSong = () => {
+    toggleSongState();
+    togglePlayButton()
   };
+  
   return (
     <div className="song bg-blk white brdr-white flex align-ctr space-bw pdg-40">
       <span>
