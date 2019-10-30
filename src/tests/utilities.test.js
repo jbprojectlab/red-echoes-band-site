cdc37e365ab972e5df1d83af313aa1a07e2f516b
James Byrd <James.Byrd@utc.com>
2019-10-30 15:12:53 -0400

    test: get album titles

--- src/tests/utilities.test.js
+++ src/tests/utilities.test.js
@@ -4,50 +4,20 @@ import {
   stopCurrentlyPlayingSongs,
 } from '../constants/utilities';
 
-const saj_M = [
-  {
-    title: 'oryn',
-    artist: 'SajMatiK',
-    album: 'saj_M',
-    src: ['/assets/songs/oryn.wav'],
-    length: '0:00',
-  },
-  {
-    title: 'sint',
-    artist: 'SajMatiK',
-    album: 'saj_M',
-    src: ['/assets/songs/sint.wav'],
-    length: '0:00',
-  },
-  {
-    title: 'pensk',
-    artist: 'SajMatiK',
-    album: 'saj_M',
-    src: ['/assets/songs/pensk.wav'],
-    length: '0:00',
-  },
-  {
-    title: 'sinuht',
-    artist: 'SajMatiK',
-    album: 'saj_M',
-    src: ['/assets/songs/sinuht.wav'],
-    length: '0:00',
-  },
-  {
-    title: 'stinarchkt',
-    artist: 'SajMatiK',
-    album: 'saj_M',
-    src: ['/assets/songs/stinarchkt.wav'],
-    length: '0:00',
-  },
-  {
-    title: 'kyhr',
-    artist: 'SajMatiK',
-    album: 'saj_M',
-    src: ['/assets/songs/kyhr.wav'],
-    length: '0:00',
-  },
-];
+import songs from '../constants/songs';
+
+const SMK = songs.filter(({album}) => album === 'SMK');
+const saj_M = songs.filter(({album}) => album === 'saj_M');
+
+const smkTitles = ['SMK'];
+const sajmatikTitles = ['saj_M'];
+
 test('get songs from specific album', () => {
+  expect(getAlbum('SMK', 'SMK')).toEqual(SMK);
   expect(getAlbum('SajMatiK', 'saj_M')).toEqual(saj_M);
+});
+
+test('get album titles from specific artist', () => {
+  expect(getAlbumTitles('SMK')).toEqual(smkTitles);
+  expect(getAlbumTitles('SajMatiK')).toEqual(sajmatikTitles);
 });
