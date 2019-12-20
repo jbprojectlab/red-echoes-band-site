14a68a5fbc3b6d3803f07c6b8c42a70f7a0dfe83
James Byrd <James.Byrd@utc.com>
2019-12-20 12:09:00 -0500

    test: header renders without crashes

---
+++ src/tests/header.test.js
@@ -0,0 +1,16 @@
+import React from 'React';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {render} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Header from '../components/header';
+
+describe('<Header />', () => {
+  test('Renders without crashing', () => {
+    const {getByText} = render(
+      <Router>
+        <Header />
+      </Router>
+    );
+    expect(getByText('seiZmiK 7')).toBeInTheDocument();
+  });
+});
